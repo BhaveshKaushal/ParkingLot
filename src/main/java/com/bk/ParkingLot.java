a444d7cb85f5f671030c39441447a7f776ee79a0
Bhavesh Kaushal <bhavesh.kaushal20@gmail.com>
2019-05-28 02:30:30 +0530

    Adding Parking lot test

--- src/main/java/com/bk/ParkingLot.java
+++ src/main/java/com/bk/ParkingLot.java
@@ -2,6 +2,17 @@ package main.java.com.bk;
 
 public class ParkingLot {
 
+    private int size;
+
     public ParkingLot(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 }
--- src/test/java/com/bk/model/test/java/com/bk/ParkingLotTest.java
+++ src/test/java/com/bk/model/test/java/com/bk/ParkingLotTest.java
@@ -3,6 +3,7 @@ package test.java.com.bk.model.test.java.com.bk;
 import main.java.com.bk.ParkingLot;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ParkingLotTest {
@@ -12,5 +13,6 @@ public class ParkingLotTest {
         int size = 10;
         ParkingLot parkinglot = new ParkingLot(size);
         assertNotNull(parkinglot);
+        assertEquals(size,parkinglot.getSize());
     }
 }
